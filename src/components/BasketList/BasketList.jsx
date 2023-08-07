624befc275173991aac2d043f0330110fb28f93d
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2023-08-07 21:44:24 +0300

    fix background img

--- src/components/BasketList/BasketList.jsx
+++ src/components/BasketList/BasketList.jsx
@@ -6,24 +6,29 @@ const BasketList = ({ myItems, removeItem }) => {
     <>
       {myItems.length > 0 &&
         myItems.map(item => (
-          <div key={item.id} className="card-basket">
-            <div className="card-basket__img-wrapper">
-              <img
-                className="image_basket"
-                src={item.image}
-                alt=""
-                height={'100%'}
-                width={'100%'}
-              />
+          <li key={item.id + Date.now()}>
+            <div className="card-basket">
+              <div className="card-basket__img-wrapper">
+                <img
+                  className="image_basket"
+                  src={item.image}
+                  alt=""
+                  height={'100%'}
+                  width={'100%'}
+                />
+              </div>
+              <div className="card-basket__text-wrapper">
+                <h2 className="card-basket__title">Бренд : {item.name}</h2>
+                <p className="card-basket__text">Ціна : {item.price}грн</p>
+              </div>
+              <Button
+                variant="outline-dark"
+                onClick={() => removeItem(item.id)}
+              >
+                Видалити з кошика
+              </Button>
             </div>
-            <div className="card-basket__text-wrapper">
-              <h2 className="card-basket__title">Бренд : {item.name}</h2>
-              <p className="card-basket__text">Ціна : {item.price}грн</p>
-            </div>
-            <Button variant="outline-dark" onClick={() => removeItem(item.id)}>
-              Видалити з кошика
-            </Button>
-          </div>
+          </li>
         ))}
     </>
   );
--- src/pages/BasketPage/BasketPage.jsx
+++ src/pages/BasketPage/BasketPage.jsx
@@ -24,29 +24,6 @@ const BasketPage = ({ myItems, total, removeItem, removeAll }) => {
           {myItems.length > 0 && (
             <BasketList myItems={myItems} removeItem={removeItem} />
           )}
-          {/* myItems.map((item) => (
-              <div className="card-basket">
-               <div className="card-basket__img-wrapper">
-                  <img
-                    className="image_basket"
-                    src={item.image}
-                    alt=""
-                    height={"100%"}
-                    width={"100%"}
-                  />
-                </div>
-                <div className="card-basket__text-wrapper">
-                  <h2 className="card-basket__title">Бренд : {item.name}</h2>
-                  <p className="card-basket__text">Ціна : {item.price}грн</p>
-                </div>
-                <Button
-                  variant="outline-dark"
-                  onClick={() => removeItem(item.id)}
-                >
-                  Видалити з кошика
-                </Button>
-              </div>
-            ))} */}
         </ul>
       </Container>
     </section>
@@ -54,14 +31,3 @@ const BasketPage = ({ myItems, total, removeItem, removeAll }) => {
 };
 
 export default BasketPage;
-
-/* <Card className="card_basket  " key={item.id}>
-  <div>
-    <Card.Img src={item.image} />
-  </div>
-  <Card.Body>
-    <Card.Title>{item.name}</Card.Title>
-    <Card.Title>{item.price}</Card.Title>
-    <Button onClick={() => removeItem(item.id)}>Видалити з кошика</Button>
-  </Card.Body>
-</Card>; */
